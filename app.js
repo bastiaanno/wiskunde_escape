f122e725bb16c34d5f58078e3f0ac33d8d44c091
Bastiaan Buitelaar <55253348+bastiaanno@users.noreply.github.com>
2021-06-02 08:09:52 +0200

    INITIAL COMMIT

---
+++ app.js
@@ -0,0 +1,69 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.Server(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const port = 3000;
+const passwords = JSON.parse(fs.readFileSync("passwords.json", 'utf-8'));
+
+const isValidJwt = (header) => {
+    const token = header.split(' ')[1];
+    if (token === 'abc') {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+app.get('/hostage', (req, res) => {
+    res.sendFile(__dirname + '/hostage.html');
+});
+app.use(express.static(__dirname + '/public'));
+
+io.use((socket, next) => {
+    const header = socket.handshake.headers['authorization'];
+    console.log(header);
+    if (isValidJwt(header)) {
+        return next();
+    }
+    return next(new Error('authentication error'));
+});
+io.on('connection', (socket) => {
+    socket.on('entry_button', (type) => {
+        if (type != 'hostage') {
+            socket.emit('password', type);
+        } else {
+            socket.emit('redirect', '/hostage');
+            console.log('redirecting');
+        }
+    });
+    socket.on('password', (type, pass) => {
+        if (type) {
+            console.log(type);
+        }
+        if (!pass || pass === undefined || pass === "" || pass === null) {
+            socket.emit('alert', 'voer een wachtwoord in!');
+            return;
+        }
+        if (pass == passwords[type]) {
+            socket.emit('alert', 'Het wachtwoord is juist!');
+        } else {
+            socket.emit('alert', 'Het wachtwoord is incorrect.');
+        }
+    });
+    console.log('a user connected');
+});
+var iosa = io.of('/hostage');
+iosa.on('connection', function(socket) {
+    console.log('A user connected to Hostage namespace');
+});
+iosa.emit('stats', { data: 'some data' });
+
+server.listen(port, () => {
+    console.log('listening on http://localhost:' + port);
+});
---
+++ public/hostage.js
@@ -0,0 +1,23 @@
+var socket = io('/hostage');
+$(document).ready(function() {
+    $("button").click(function() {
+        socket.emit("entry_button", this.id);
+    });
+    socket.on("password", function(type) {
+        var password = prompt("wat is het wachtwoord?");
+        socket.emit('password', type, password);
+    });
+    socket.on('alert', (incoming) => {
+        alert(incoming);
+    });
+    socket.on("disconnect", (reason) => {
+        console.log('disconnected');
+    });
+    socket.on("reconnect", () => {
+        console.log('reconnected');
+    });
+    socket.on('redirect', (url) => {
+        console.log(url);
+        window.location.replace(url);
+    })
+});
