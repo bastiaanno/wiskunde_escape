f4885b8e6f3aa803465738248e040c3533e90efa
Bastiaan Buitelaar <55253348+bastiaanno@users.noreply.github.com>
2021-07-04 11:53:53 +0200

    Update mobile.js

---
+++ public/mobile.js
@@ -0,0 +1,56 @@
+var socket = io('/super');
+var chatSocket = io('/chat');
+const disable_marker = false;
+$(document).ready(function() {
+    socket.on("password", function(type) {
+        var password = prompt("wat is het wachtwoord?");
+        socket.emit('password', type, password);
+    });
+    socket.on('alert', (incoming) => {
+        alert(incoming);
+    });
+    socket.on("disconnect", (reason) => {
+        console.log('disconnected');
+    });
+    socket.on("reconnect", () => {
+        console.log('reconnected');
+    });
+    socket.on('redirect', (url) => {
+        console.log(url);
+        window.location.replace(url);
+    });
+    var messages = document.getElementById('messages');
+    var form = document.getElementById('form');
+    var input = document.getElementById('input');
+
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        if (input.value) {
+            chatSocket.emit('chat message', input.value, "Mobiele Ploeg", $.now());
+            input.value = '';
+        }
+    });
+
+    chatSocket.on('chat message', function(msg, sender, timestamp) {
+        var item = document.createElement('li');
+        item.textContent = formatTime(timestamp) + " [" + sender + "]: " + msg;
+        messages.appendChild(item);
+        //window.scrollTo(0, document.body.scrollHeight);
+        $('#messages').scrollTop($('#messages')[0].scrollHeight);
+    });
+
+    function formatTime(timestamp) {
+        // Create a new JavaScript Date object based on the timestamp
+        var date = new Date(timestamp);
+        // Hours part from the timestamp
+        var hours = date.getHours();
+        // Minutes part from the timestamp
+        var minutes = "0" + date.getMinutes();
+        // Seconds part from the timestamp
+        var seconds = "0" + date.getSeconds();
+
+        // Will display time in 10:30:23 format
+        var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+        return formattedTime;
+    }
+});
